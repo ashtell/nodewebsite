e0fed0815c24cbf3457c1470ee3239adec73c490
Michael Hall <michael@localhost.localdomain>
2017-04-15 17:45:10 -0600

    initial commit

--- Readme.md
+++ Readme.md
@@ -1,18 +1,25 @@
-# Basic Express Site (2016 Edition)
 
 A simple website in node js to accompany a blog post.
 
 ## Setup
 
 ```
-npm i
 npm run build
 npm start
-
+---- WHEN NOT USING ON DEDICATED SERVER ----
 # development mode (watches for source changes)
 npm run watch
 ```
-
-## Screenshot
-
-![Screenshot](https://raw.githubusercontent.com/bengourley/basic-express-site-2016/master/screenshot.png)
+in Order to make a new page
+1. Make the page at --source--templates--example.jade
+2. in ```server.js``` at the top put ```, examplePage = require('jade').compileFile(__dirname + '/source/templates/example.jade');```
+3. then still in ```server.js``` after the above line you just created add this ``` app.get('/exampleSubject', function(req, res, next) {
+                                                                                        try {
+                                                                                            var html = examplePage({title: 'exampleTitle' });
+                                                                                            res.send(html);
+                                                                                            
+                                                                                        } catch (e) {
+                                                                                            next(e);
+                                                                                        }
+                                                                                    });
+```
--- server.js
+++ server.js
@@ -1,20 +1,79 @@
-var express = require('express')
-  , logger = require('morgan')
-  , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+var express = require('express'),
+    logger = require('morgan'),
+    app = express(),
+    home = require('jade').compileFile(__dirname + '/source/templates/homepage.jade'),
+    contact = require('jade').compileFile(__dirname + '/source/templates/contact.jade'),
+    news = require('jade').compileFile(__dirname + '/source/templates/news.jade'),
+    aboutMe = require('jade').compileFile(__dirname + '/source/templates/about.jade'),
+    march31 = require('jade').compileFile(__dirname + '/source/templates/blog/2017/march/31.jade'),
+    april9 = require('jade').compileFile(__dirname + '/source/templates/blog/2017/april/9.jade');
 
-app.use(logger('dev'))
-app.use(express.static(__dirname + '/static'))
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/static'));
 
-app.get('/', function (req, res, next) {
+app.get('/blog/2017/april/9', function(req, res, next) {
   try {
-    var html = template({ title: 'Home' })
-    res.send(html)
-  } catch (e) {
-    next(e)
+    var html = april9({
+        title: 'april 9th'
+    });
+    res.send(html);
+  } catch (e){
+    next(e);
   }
-})
+});
+app.get('/blog/2017/march/31', function(req, res, next) {
+  try {
+    var html = march31({
+        title: 'March 31st'
+    });
+    res.send(html);
+  } catch (e){
+    next(e);
+  }
+});
+app.get('/about', function(req, res, next) {
+    try {
+        var html = aboutMe({
+            title: 'About'
+        });
+        res.send(html);
+    } catch (e) {
+        next(e);
+    }
+});
+app.get('/news', function(req, res, next) {
+    try {
+        var html = news({
+            title: 'News'
+        });
+        res.send(html);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
-})
+    } catch (e) {
+        next(e);
+    }
+});
+app.get('/contact', function(req, res, next) {
+    try {
+        var html = contact({
+            title: 'Contact'
+        });
+        res.send(html);
+
+    } catch (e) {
+        next(e);
+    }
+});
+app.get('/', function(req, res, next) {
+    try {
+        var html = home({
+            title: 'Home'
+        });
+        res.send(html);
+    } catch (e) {
+        next(e);
+    }
+});
+
+app.listen(process.env.PORT || 3050, function() {
+    console.log('Listening on http://localhost:' + (process.env.PORT || 3000));
+});
